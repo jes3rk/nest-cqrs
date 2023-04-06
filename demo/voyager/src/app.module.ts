e3a1badd45c94a7ab64b32cc84f5cdbd069af999
jes3rk <joseph.schefer@gmail.com>
2023-04-06 15:03:55 +0000

    setup ci and releases

--- demo/voyager/src/app.module.ts
+++ demo/voyager/src/app.module.ts
@@ -1,25 +1,45 @@
 import { Module } from "@nestjs/common";
 import { MessengerModule } from "nest-messenger";
 import { AppController } from "./app.controller";
-import { CQRSModule } from "@nest-cqrs/core";
+import { CQRSModule, PluginConfiguration } from "@nest-cqrs/core";
 import { configureEventStoreDB } from "@nest-cqrs/eventstoredb";
 import { HotelModule } from "./hotel/hotel.module";
 import { VoyageModule } from "./voyage/voyage.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { configureNats } from "@nest-cqrs/nats";
 
-@Module({
-  imports: [
-    HotelModule,
-    CQRSModule.forRoot({
-      applicationName: "voyager",
-      eventStoreConfig: configureEventStoreDB({
+const generateEventStoreConfig = (): PluginConfiguration => {
+  const storeType = process.env.STORE_TYPE || "eventstoredb";
+  switch (storeType) {
+    case "eventstoredb":
+      return configureEventStoreDB({
         eventStoreConfig: {
           useValue: {
             connectionString:
               "esdb://localhost:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000",
           },
         },
-      }),
+      });
+    case "nats":
+      return configureNats({
+        eventStoreConfig: {
+          useValue: {
+            connection: {
+              servers: "localhost",
+            },
+          },
+        },
+      });
+    default:
+      throw new Error("Unsupported event store");
+  }
+};
+@Module({
+  imports: [
+    HotelModule,
+    CQRSModule.forRoot({
+      applicationName: "voyager",
+      eventStoreConfig: generateEventStoreConfig(),
     }),
     MessengerModule,
     TypeOrmModule.forRoot({
